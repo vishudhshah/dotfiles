a77f80342dd03f62fdef9e5ec24869a38af6800b
Vishudh Shah <vishudh@hotmail.com>
2026-03-12 01:57:11 -0700

    modify git.yazi sign styles

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,6 @@
 -- git.yazi plugin
 th.git = th.git or {}
-th.git.untracked_sign = "U"
+th.git.untracked_sign = "✗"
 th.git.updated_sign = "C"
 th.git.clean_sign = "✓"
 th.git.clean = ui.Style():fg("green")
