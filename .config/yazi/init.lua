cf6a549d873de7a502bb6bcfc32b95b321e5d785
Vishudh Shah <vishudh@hotmail.com>
2026-03-12 02:41:43 -0700

    yazi full-border plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,4 +1,4 @@
--- git.yazi plugin
+-- git plugin
 th.git = th.git or {}
 th.git.untracked_sign = "✗"
 th.git.updated_sign = "C"
@@ -7,4 +7,10 @@ th.git.clean = ui.Style():fg("green")
 require("git"):setup {
 	-- Order of status signs showing in the linemode
 	order = 1500,
+}
+
+-- full-border plugin
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
 }
