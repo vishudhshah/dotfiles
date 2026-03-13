59145a0d8e561a0412f8e459d39c0bcc0b139916
Vishudh Shah <vishudh@hotmail.com>
2026-03-12 23:24:54 -0700

    added smear-cursor nvim plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -94,4 +94,16 @@ return {
     version = "*",
     opts = {},
   },
+
+  -- smear-cursor for smooth cursor movement
+  {
+    "sphamba/smear-cursor.nvim",
+    lazy = false,
+    opts = {
+      stiffness = 0.8,
+      trailing_stiffness = 0.7,
+      matrix_pixel_threshold = 0.5,
+      cursor_color = "#5fb3f9",
+    },
+  }
 }
