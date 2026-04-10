53f88bfc1e3c24e6d46d1cdf69d5fbae72ea00b2
Vishudh Shah <vishudh@hotmail.com>
2026-04-10 14:31:12 -0700

    render-markdown.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -221,4 +221,12 @@ return {
       })
     end,
   },
+
+  -- render-markdown.nvim for markdown rendering in Neovim
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    ft = { "markdown" },
+    opts = {},
+  },
 }
