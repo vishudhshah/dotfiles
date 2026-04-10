b076b146e5c3a6101f03e68a313dad351746367d
Vishudh Shah <vishudh@hotmail.com>
2026-04-10 16:29:38 -0700

    TODO highlight plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -229,4 +229,20 @@ return {
     ft = { "markdown" },
     opts = {},
   },
+
+  -- todo-comments.nvim for highlighting and searching TODO comments
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    opts = {
+      highlight = {
+        keyword = "bg",
+        after = "",
+      },
+      colors = {
+        info = { "#00cdfe" },  -- cadetblue, soft and muted
+      },
+    }
+  }
 }
