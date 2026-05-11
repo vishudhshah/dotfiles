eaaa30cda2c2b9966de4ccf7b7783eba733b6384
Vishudh Shah <vishudh@hotmail.com>
2026-05-11 14:29:46 -0700

    update aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,16 +1,15 @@
 alias clear='clear && printf "\e[3J"'
-alias code="code-insiders"
-alias ls="eza --icons --tree --level=1"
-alias lsa="eza --icons --tree --level=1 --all"
-alias tree="eza --icons --tree"
+alias code=code-insiders
+alias ls='eza --icons --tree --level=1'
+alias lsa='eza --icons --tree --level=1 --all'
+alias tree='eza --icons --tree'
 alias vim=nvim
 alias vi=nvim
 alias cat=bat
 alias catp='bat --paging=never'
 alias cheat='~/.config/cheat/cheat.sh'
-alias ff="clear;fastfetch"
+alias ff='clear && fastfetch'
 alias q='exit'
-alias cdh="cd ~"
 alias spotify=spotify_player
 alias cl='clear'
 
