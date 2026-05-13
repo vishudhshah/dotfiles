0522272f03ba16a94e3884fff1761f972ff07ed2
Vishudh Shah <vishudh@hotmail.com>
2026-05-12 17:33:13 -0700

    lazygit alias + sort aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,20 +1,21 @@
-alias clear='clear && printf "\e[3J"'
-alias code=code-insiders
-alias ls='eza --icons --tree --level=1'
-alias lsa='eza --icons --tree --level=1 --all'
-alias tree='eza --icons --tree'
-alias vim=nvim
-alias vi=nvim
 alias cat=bat
 alias catp='bat --paging=never'
 alias cheat='~/.config/cheat/cheat.sh'
+alias cl=clear
+alias clear='clear && printf "\e[3J"'
+alias code=code-insiders
 alias ff='clear && fastfetch'
+alias lg=lazygit
+alias ls='eza --icons --tree --level=1'
+alias lsa='eza --icons --tree --level=1 --all'
 alias q='exit'
 alias spotify=spotify_player
-alias cl='clear'
+alias tree='eza --icons --tree'
+alias vi=nvim
+alias vim=nvim
 
 # Telegram expenses bot
+alias expenses-logs='tail -f ~/Library/Logs/expenses-bot.log'
 alias expenses-restart='launchctl unload ~/Library/LaunchAgents/com.vishudh.expenses-bot.plist && launchctl load ~/Library/LaunchAgents/com.vishudh.expenses-bot.plist'
 alias expenses-start='launchctl load ~/Library/LaunchAgents/com.vishudh.expenses-bot.plist'
 alias expenses-stop='launchctl unload ~/Library/LaunchAgents/com.vishudh.expenses-bot.plist'
-alias expenses-logs='tail -f ~/Library/Logs/expenses-bot.log'
