a9e333b47937ee9d73f41a8712aca9bf519bce36
Vishudh Shah <vishudh@hotmail.com>
2026-05-26 19:12:43 -0700

    fix first space press issue

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -39,3 +39,6 @@ map("n", "<leader>gl", function() Snacks.lazygit.log() end, { desc = "Lazygit lo
 map("n", "<leader>gf", function() Snacks.lazygit.log_file() end, { desc = "Lazygit file log" })
 pcall(vim.keymap.del, "n", "<leader>ds")
 map("n", "<leader>d", function() Snacks.dashboard.open() end, { desc = "Dashboard" })
+
+-- Suppress default space key behavior in normal and visual modes
+map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,5 @@
+-- Not lazy loaded by deafult in NvChad
+return {
+  "folke/which-key.nvim",
+  lazy = false,
+}
