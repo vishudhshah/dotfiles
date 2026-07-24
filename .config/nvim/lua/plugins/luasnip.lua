dfe2d6ef8538f8440c52166b352d3b26610fa181
Vishudh Shah <vishudh@hotmail.com>
2026-07-24 20:07:08 +0530

    fix dead tab - now inserts spaces

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -28,6 +28,15 @@ return {
       if ls.jumpable(-1) then ls.jump(-1) end
     end, { silent = true })
 
+    -- Insert spaces when there's no snippet
+    vim.keymap.set({ "i", "s" }, "<Tab>", function()
+      if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+      end
+    end, { silent = true })
+
     -- Cycle through choice nodes
     vim.keymap.set({ "i", "s" }, "<C-e>", function()
       if ls.choice_active() then ls.change_choice(1) end
